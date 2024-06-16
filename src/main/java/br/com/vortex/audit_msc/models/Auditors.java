63eef6f5e89b47662305c0f645bf0e8a936f1f48
Rod <bezerra07.rodrigo@gmail.com>
2024-06-16 17:42:30 -0300

    feat: improve json responses.

--- src/main/java/br/com/vortex/audit_msc/models/Auditors.java
+++ src/main/java/br/com/vortex/audit_msc/models/Auditors.java
@@ -1,8 +1,7 @@
 package br.com.vortex.audit_msc.models;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
