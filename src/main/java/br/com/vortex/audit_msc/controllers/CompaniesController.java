77056d6f67073f659198a9a6bd5c87614d937c47
Rod <bezerra07.rodrigo@gmail.com>
2024-07-09 18:26:06 -0300

    feat: add delete and edit functions to companies.

--- src/main/java/br/com/vortex/audit_msc/controllers/CompaniesController.java
+++ src/main/java/br/com/vortex/audit_msc/controllers/CompaniesController.java
@@ -1,14 +1,12 @@
 package br.com.vortex.audit_msc.controllers;
 
+import br.com.vortex.audit_msc.exceptions.ResourceNotFoundException;
 import br.com.vortex.audit_msc.models.Companies;
 import br.com.vortex.audit_msc.services.CompaniesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,21 @@ public class CompaniesController {
     public ResponseEntity<Companies> createCompany(@RequestBody Companies company) {
         Companies newCompany = companiesService.createCompany(company);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCompany);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Companies> deleteCompany(@PathVariable Integer id) {
+        companiesService.deleteCompany(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("edit/{id}")
+    public ResponseEntity<?> editCompany(@PathVariable Integer id, @RequestBody Companies updatedCompany) {
+        try {
+            companiesService.editCompany(id, updatedCompany);
+            return ResponseEntity.ok(updatedCompany);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
--- src/main/java/br/com/vortex/audit_msc/services/CompaniesService.java
+++ src/main/java/br/com/vortex/audit_msc/services/CompaniesService.java
@@ -1,5 +1,6 @@
 package br.com.vortex.audit_msc.services;
 
+import br.com.vortex.audit_msc.exceptions.ResourceNotFoundException;
 import br.com.vortex.audit_msc.models.Companies;
 import br.com.vortex.audit_msc.repositories.CompaniesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,52 @@ public class CompaniesService {
 
     public Optional<Companies> findById(Integer id) {
         return companiesRepository.findById(id);
+    }
+
+    public void deleteCompany(Integer id) {
+        if (!companiesRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Standard not found with id " + id );
+        }
+        companiesRepository.deleteById(id);
+    }
+
+    public Companies editCompany(Integer id, Companies updatedCompany) {
+        Optional<Companies> company = companiesRepository.findById(id);
+        if (company.isPresent()) {
+            Companies existingCompany = company.get();
+
+            if (updatedCompany.getName() != null) {
+                existingCompany.setName(updatedCompany.getName());
+            }
+            if (updatedCompany.getAddress() != null) {
+                existingCompany.setAddress(updatedCompany.getAddress());
+            }
+            if (updatedCompany.getCnpj() != null) {
+                existingCompany.setCnpj(updatedCompany.getCnpj());
+            }
+            if (updatedCompany.getContact_name() != null) {
+                existingCompany.setContact_name(updatedCompany.getContact_name());
+            }
+            if (updatedCompany.getContact_number() != null) {
+                existingCompany.setContact_number(updatedCompany.getContact_number());
+            }
+            if (updatedCompany.getScope() != null) {
+                existingCompany.setScope(updatedCompany.getScope());
+            }
+            if (updatedCompany.getIs_intercompany() != null) {
+                existingCompany.setIs_intercompany(updatedCompany.getIs_intercompany());
+            }
+            if (updatedCompany.getIs_active() != null) {
+                existingCompany.setIs_active(updatedCompany.getIs_active());
+            }
+            if (updatedCompany.getStandard_code() != null) {
+                existingCompany.setStandard_code(updatedCompany.getStandard_code());
+            }
+
+            return companiesRepository.save(existingCompany);
+        } else {
+            throw new ResourceNotFoundException("Company not found with id " + id);
+        }
     }
 
 }
